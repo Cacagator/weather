6b27e46aa464b0d3ada3044cd0ec52fbf798adc0
Miao Sun <cactus@i21a-s1.ufhpc>
2016-09-17 15:55:36 -0400

    add curl -s to silent

--- weather.sh
+++ weather.sh
@@ -6,7 +6,7 @@ yesterday=`date -d yesterday '+%Y/%m/%d'`
 
 #echo "https://www.wunderground.com/history/airport/GNV/$yesterday/DailyHistory.heml?&format=1"|curl - >gnv.txt
 
-curl `echo "https://www.wunderground.com/history/airport/GNV/$yesterday/DailyHistory.heml?&format=1"` >gnv.txt
+curl -s `echo "https://www.wunderground.com/history/airport/GNV/$yesterday/DailyHistory.heml?&format=1"` >gnv.txt
 
 maxTem=`cut -f2 -d ',' gnv.txt|sort -n|tail -1`
 
