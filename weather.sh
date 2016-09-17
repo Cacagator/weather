a5b7daefd2ca98588ee5661bc4c99e3b3cb359af
Miao Sun <cactus@i21a-s1.ufhpc>
2016-09-17 16:02:50 -0400

    add user defined airport code variable

--- weather.sh
+++ weather.sh
@@ -1,14 +1,18 @@
 #! /bin/bash
 
+echo "Which airport you want to check?"
+
+read code
+
 yesterday=`date -d yesterday '+%Y/%m/%d'`
 
 #website="https://www.wunderground.com/history/airport/GNV/2016/09/06/DailyHistory.heml?&format=1"
 
 #echo "https://www.wunderground.com/history/airport/GNV/$yesterday/DailyHistory.heml?&format=1"|curl - >gnv.txt
 
-curl -s `echo "https://www.wunderground.com/history/airport/GNV/$yesterday/DailyHistory.heml?&format=1"` >gnv.txt
+curl -s `echo "https://www.wunderground.com/history/airport/$code/$yesterday/DailyHistory.heml?&format=1"` >gnv.txt
 
 maxTem=`cut -f2 -d ',' gnv.txt|sort -n|tail -1`
 
-echo Yesterday is $yesterday, and the Max Temp is $maxTem degrees Fahrenheit!
+echo Yesterday is $yesterday, and the Max Temp at $code is $maxTem degrees Fahrenheit!
 
