9a849b2be64f591940401cc4358067f0046abc75
Miao Sun <cactus@i21a-s2.ufhpc>
2016-09-21 10:46:10 -0400

    revised for yesterday

--- weather.sh
+++ weather.sh
@@ -4,22 +4,25 @@ echo "Which airport do you want to check?"
 
 read code
 
-#yesterday=`date -d yesterday '+%Y/%m/%d'`
+yesterday=`date -d yesterday '+%Y/%m/%d'`
 
-Year=`date '+%Y/%m/%d'|cut -f1 -d '/'`
+#Year=`date '+%Y/%m/%d'|cut -f1 -d '/'`
 
-Month=`date '+%Y/%m/%d'|cut -f2 -d '/'`
+#Month=`date '+%Y/%m/%d'|cut -f2 -d '/'`
 
-Day=`date '+%Y/%m/%d'|cut -f3 -d '/'`
+#Day=`date '+%Y/%m/%d'|cut -f3 -d '/'`
 
 #yesterday=$Day - 1
 
 #echo "https://www.wunderground.com/history/airport/GNV/$yesterday/DailyHistory.heml?&format=1"|curl - >$code.txt
 
-curl -s `echo "https://www.wunderground.com/history/airport/$code/$Year/$Month/$Day/DailyHistory.heml?&format=1"` >$code.txt
+#curl -s `echo "https://www.wunderground.com/history/airport/$code/$Year/$Month/$Day/DailyHistory.heml?&format=1"` >$code.txt
+
+curl -s `echo "https://www.wunderground.com/history/airport/$code/$yesterday/DailyHistory.heml?&format=1"` >$code.txt
 
 maxTem=`cut -f2 -d ',' gnv.txt|sort -n|tail -1`
 
-echo Yesterday is $Day-$Month-$Year, and the Max Temp at $code is $maxTem degrees Fahrenheit!
+#echo Yesterday is $Day-$Month-$Year, and the Max Temp at $code is $maxTem degrees Fahrenheit!
 
+echo Yesterday is $yesterday, and the Max Temp at $code is $maxTem degrees Fahrenheit!
 
